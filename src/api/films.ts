f8da820c0c8647c5c4766aeddef8099818161702
Marek Sirakov <sirm05@vse.cz>
2026-05-22 15:35:09 +0200

    Úkol 6 - funkci na fetch z jasonu jsem hodil do api/film.ts

--- src/api/films.ts
+++ src/api/films.ts
@@ -2,6 +2,6 @@ import type { Film } from '../types/film.types';
 
 export default async function fetchFilms() {
   const res = await fetch('/films.json');
-  if (!res.ok) throw new Error('Nepodařilo se načíst filmy');
+  if (!res.ok) throw new Error('Nepodařilo se načíst filmy ${res.status}');
   return res.json() as Promise<Film[]>;
 }
